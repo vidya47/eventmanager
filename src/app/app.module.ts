d2a1be94cd55d64bee90656ad1b955e32d08a810
vidya47 <vidya47@gmail.com>
2018-03-07 15:24:26 -0500

    An event manager where events can be added from an authorized user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
+import { AuthModule } from './auth/auth.module';
+import { HeaderComponent } from './header/header.component';
+import { EventModule } from './event/event.module';
+import { FormioResources } from 'angular-formio/resource';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: () => AuthModule
+      },
+      {
+        path: 'event',
+        loadChildren: () => EventModule
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    FormioResources,
+    FormioAuthService,
+    {provide: FormioAuthConfig, useValue: AuthConfig},
+    {provide: FormioAppConfig, useValue: AppConfig}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
